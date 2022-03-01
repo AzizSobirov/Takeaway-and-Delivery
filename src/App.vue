43f6bda2fb26aff806a9e7eee957adf94e30eae5
Aziz Sobirov <azizsobirovdev@gmail.com>
2022-03-02 01:48:47 +0500

    2.03

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <the-nav></the-nav>
+  <the-home></the-home>
+  <the-products></the-products>
+</template>
+<script>
+import TheNav from "./components/TheNav.vue";
+import TheHome from "./views/Home.vue";
+import TheProducts from "./views/TheProducts.vue";
+import "./css/app.css";
+export default {
+  components: {
+    TheNav,
+    TheHome,
+    TheProducts
+  },
+};
+</script>
